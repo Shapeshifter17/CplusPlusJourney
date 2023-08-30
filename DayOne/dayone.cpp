9abf2e9316f9d7008ae44a313888c2fb639c3324
Andrei <andreibarbulescu17@gmail.com>
2023-08-29 22:17:03 -0400

    day1

--- DayOne/dayone.cpp
+++ DayOne/dayone.cpp
@@ -1,22 +1,48 @@
+
 #include <iostream>
 #include <fstream>
+
 #include <string>
+#include <vector>
 using namespace std;
 
 int main(){
-	cout << "Hi, will read the input file and output the biggest number";
+	cout << "Hi, will read the input file and output the biggest number" << endl;
 	fstream newfile;
 	newfile.open("input.txt",ios::in);
+	vector<int> list;
 
 	if(newfile.is_open()){
 		string tp;
-		int max = 0;
 		int num;
-		while(getline(newfile,num)){
-			cout << tp << "\n";
-			max += num;
+		
+		while(getline(newfile,tp)){
+			if(tp.empty()){
+				list.push_back(0);}	
+			else{
+				num = std::stoi(tp);
+				list.push_back(num);}
+		
 		}
-		newfile.close();}
-}
+		newfile.close();
+
+	int max = 0;
+	int temp = 0;	
+	for (int i=0;i<list.size();i++) {
+		if(list[i] == 0){
+			if(max < temp){
+				max = temp;
+				}	
+			temp = 0;}
+		else{
+			temp += list[i];}
+	
+	}
+		if (max < temp){
+			max = temp;}
+std::cout << max << std::endl;
+std::cout << "this is my first time using nvim!" << std::endl;
+}}	
+
 
 
