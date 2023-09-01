4730b3c53d3f38834450dbb06b5120c285f24006
Andrei <andreibarbulescu17@gmail.com>
2023-09-01 11:37:56 -0400

    formatted code

--- DayOne/dayone.cpp
+++ DayOne/dayone.cpp
@@ -15,34 +15,34 @@ int main(){
 	if(newfile.is_open()){
 		string tp;
 		int num;
-		
+
 		while(getline(newfile,tp)){
 			if(tp.empty()){
-				list.push_back(0);}	
+			list.push_back(0);}	
 			else{
 				num = std::stoi(tp);
-				list.push_back(num);}
-		
-		}
-		newfile.close();
+			list.push_back(num);}
 
-	int max = 0;
-	int temp = 0;	
-	for (int i=0;i<list.size();i++) {
-		if(list[i] == 0){
-			if(max < temp){
-				max = temp;
+		}
+	newfile.close();
+
+		int max = 0;
+		int temp = 0;	
+		for (int i=0;i<list.size();i++) {
+			if(list[i] == 0){
+				if(max < temp){
+					max = temp;
 				}	
 			temp = 0;}
-		else{
+			else{
 			temp += list[i];}
-	
-	}
-		if (max < temp){
-			max = temp;}
-std::cout << max << std::endl;
-std::cout << "this is my first time using nvim!" << std::endl;
-}}	
+
+		}
+	if (max < temp){
+		max = temp;}
+		std::cout << max << std::endl;
+		std::cout << "this is my first time using nvim!" << std::endl;
+	}}	
 
 
 
