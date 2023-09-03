f4caa52924364e6137ced77bbaf6101c02ab9248
Andrei <andreibarbulescu17@gmail.com>
2023-09-03 14:03:29 -0400

    beginning of simple tic tac to

---
+++ TicTacToe/main.cpp
@@ -0,0 +1,66 @@
+#include <array>
+#include <cstdlib>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#define SIZE  9
+int initialSetup();
+std::array<std::string, SIZE> initializeBoard();
+void printBoard(std::array<std::string,SIZE>& board);
+int main () {
+	
+	int randomNum = initialSetup();
+	int input;
+	std::array<std::string,SIZE>board = initializeBoard();
+
+	while (true) {
+		std::cin >> input;
+		input --;
+		switch (randomNum) {
+			case 1:
+				board[input] = "X";
+				randomNum = 2;		
+			break;
+			case 2:
+				board[input] = "0";
+				randomNum = 1;		
+			break;
+		}
+
+	printBoard(board);}
+	return 0; }
+
+
+
+int initialSetup(){
+	srand(time(0));	
+	int randomNum = 1 + rand()% 2; std::cout << "Welcome to Tic-Tac-Toe" << std::endl; std::cout << "The coinflip will decide if player 1 or player 2 starts" << std::endl;
+	std::cout << "press a number from 1 to 9 to mark your spot \nPLayer 1 is X and Player 2 is 0" << std::endl;
+	std::cout << "Player "<<randomNum << " will start" << std::endl;
+	return randomNum;
+}
+
+
+std::array<std::string, SIZE> initializeBoard(){
+	std::array<std::string, SIZE> board;
+	for (int i= 0; i < SIZE; i++) {
+		board[i] = " ";
+	}
+	return board;
+}
+
+void printBoard(std::array<std::string,SIZE>& board){
+	std::string firstInput;
+	std::string secondInput;
+	std::string thirdInput;
+	firstInput = " " + board[0] + " | " + board[1] + " | " + board[2];
+
+	secondInput = " " + board[3] + " | " + board[4] + " | " + board[5];
+	thirdInput = " " + board[6] + " | " + board[7] + " | " + board[8];
+	std::cout << firstInput << std::endl;
+	std::cout << secondInput << std::endl;
+	std::cout << thirdInput << std::endl;
+	return;}
+
+
+
