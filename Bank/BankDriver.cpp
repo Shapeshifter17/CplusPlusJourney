56c6a4d6aa9525f0c729fa949f7a93ee2882a1a9
Andrei Barbulescu <andreibarbulescu17@gmail.com>
2023-10-25 16:11:38 -0400

    Update BankDriver.cpp

--- Bank/BankDriver.cpp
+++ Bank/BankDriver.cpp
@@ -8,31 +8,39 @@ using std::string;
 using std::ifstream;
 using std::ofstream;
 
-void Printmenu();
+void printWelcomeMenu();
+void printUserMenu();
+
+string path = "InfoFiles/Users/";
+
 
 int main(int argc, char const *argv[])
 {
     string userResponse;
 
-    Printmenu();
+    printWelcomeMenu();
 
     do{
-
-         cout << "Are you a user (1) or an employee (2) or you want to exit (0) " << "\n" ;
+        cout << "Are you a user (1) or an employee (2) or you want to exit (0) " << "\n" ;
         getline(cin,userResponse);
    
     }while(userResponse != "1" && userResponse !="2");
     
-    string path = "InfoFiles/Users/11111111.txt";
+ 
+    string userAcc;
     if (userResponse == "1")
     {
         cout << "Welcome User, please identify yourself with your Bank Account Number" << "\n";
-        
+        getline(cin,userAcc);
+        cout << "Account information has been found" << "\n";
+        printUserMenu();
     }
-    
+
+    path.append(userAcc);
+    path.append(".txt");
     
     ifstream file;
-    file.open("InfoFiles/Users/11111111.txt");
+    file.open(path);
     string text;
 
     while (getline (file, text)) {
@@ -43,17 +51,24 @@ int main(int argc, char const *argv[])
 
     file.close();
 
-    ofstream wFile;
-    wFile.open("InfoFiles/Users/11111111.txt", ofstream::app);
-
-    wFile << "dubadabadu";
-
-    wFile.close();
+   // ofstream wFile;
+   // wFile.open("InfoFiles/Users/11111111.txt", ofstream::app);
+   // wFile.close();
     return 0;
 }
 
-void Printmenu(){
+void printWelcomeMenu(){
     cout << "Welcome to my Bank Account Application!! \n";
-    cout << "======================================== \n";
-    
+    cout << "======================================== \n"; 
+}
+
+void printUserMenu(){
+
+    cout << "What would you want to do? \n";
+    cout << "1. View your transaction history \n";
+    cout << "2. Add a deposit \n";
+    cout << "3. Make a withdraw \n";
+    cout << "4. Create an account \n";
+    cout << "0. Exit \n";
+
 }
