9eb2c446759525caf801358ab26040c74bb7ac88
Andrei Barbulescu <andreibarbulescu17@gmail.com>
2023-10-25 13:41:13 -0400

    Create User.h

---
+++ Bank/User.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using std::string;
+
+class User{
+
+    private:
+    string name;
+    double balance;
+    int bankAccountNumber;
+    enum Type{
+        student = 0;
+        normal = 1;
+        privileged = 2;
+    };
+
+    public:
+    User();
+    User (string name, double balance, int bankAccountNumber, int type)
+
+
+}
