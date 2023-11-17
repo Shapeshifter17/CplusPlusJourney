f5993b0daba9085917d5259e022855bb956796a7
Andrei <andreibarbulescu17@gmail.com>
2023-11-17 12:21:04 -0500

    two sum

---
+++ LeetcodePractice/TwoSum/main.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+
+using std::vector;
+
+class Solution {
+public:
+
+	//Speed is O(n^2) because of the two for loops, not very eefficient
+   /* vector<int> twoSum(vector<int>& nums, int target) {
+        int num1 = 0;
+        int num2 = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+		for (int j = 0; j < i; j++) {
+			if (nums[i] + nums[j] == target) {
+				return vector<int> {i,j};
+			}
+		}
+	}
+return vector<int>();
+			        
+    }
+*/
+	vectr<int> twoSum(vector<int>& nums, int target){
+
+		
+	}
+
+
+};
